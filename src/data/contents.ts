1c0f5d799714539b99a003cea0624ff55a30366b
LeudiX <leudix.rafael@gmail.com>
2025-03-27 13:37:50 -0400

    fix(media): Updated projects media dataset extensions and refs for better system performance

--- src/data/contents.ts
+++ src/data/contents.ts
@@ -1,40 +1,117 @@
-//? Logo icons import
-import racine_halftone from "../assets/images/logo/racine_halftone.png"
-import racine_halftonewhite from "../assets/images/logo/racine_halftonewhite.png"
+//! Logo icons import
+import racine_halftone from "/media/logo/racine_halftone.webp"
+import racine_halftonewhite from "/media/logo/racine_halftonewhite.webp"
 
-//? Hero section main pictures import
-import tracers_logo from "../assets/images/hero/Tracers Logo.png";
-import racine_mask from "../assets/images/hero/racine_mask.webp";
+//! Hero section main pictures import
+import tracers_logo from "/media/hero/tracers_logo.webp";
+import racine_mask from "/media/hero/racine_mask.webp";
 
-//? Gallery section portrait images import
-import dorian_portrait from "/media/gallery/dorian.png";
-import teezo_portrait from "/media/gallery/teezo.png";
-import mattox_portrait from "/media/gallery/mattox.png";
-import lilnas_portrait from "/media/gallery/lil_nas.png";
-import others_portrait from "/media/gallery/others.png";
-import paper_mag from "/media/gallery/paper_mag.png";
+//! Gallery section portrait images import
+import dorian_portrait from "/media/gallery/dorian.webp";
+import teezo_portrait from "/media/gallery/teezo.webp";
+import mattox_portrait from "/media/gallery/mattox.webp";
+import lilnas_portrait from "/media/gallery/lil_nas.webp";
+import others_portrait from "/media/gallery/others.webp";
+import paper_mag from "/media/gallery/paper_mag.webp";
 
+//! About section required parallax images import
+import parallax_leafs from "/media/about/parallax_leafs_cloud.webp";
+import parallax_arm_blue from "/media/about/parallax_arm_blue.webp";
+import parallax_park from "/media/about/parallax_park_cloud.webp";
+import xavier_cloud from "/media/about/xavier_cloud.webp";
+import arm from "/media/about/arm.webp";
+import black_person_cut from "/media/about/black_person_cut.webp";
 
-//? Carousel media content imports !!!
+//! CAROUSEL MEDIA IMPORTS!!!
 
-//? LIL NAS X [MEDIA]
-  //? Eric Andre Show!!
+//! LIL NAS X [MEDIA]
+//? Eric Andre Show!!
 import eric_andre_video_url from "/media/lil_nasx/lilnasx_eric_andre.mp4";
-import eric_andre_img1_url from "/media/lil_nasx/lilnasx_updated/1.jpg";
-import eric_andre_img2_url from "/media/lil_nasx/lilnasx_updated/2.jpg";
-import eric_andre_img3_url from "/media/lil_nasx/lilnasx_updated/3.jpg";
+import eric_andre_img1_url from "/media/lil_nasx/lilnasx_updated/1.webp";
+import eric_andre_img2_url from "/media/lil_nasx/lilnasx_updated/2.webp";
+import eric_andre_img3_url from "/media/lil_nasx/lilnasx_updated/3.webp";
 
-  //? Vitamin Water Ad!!
+//? Vitamin Water Ad!!
 import vitamin_water_img1_url from "/media/lil_nasx/1.jpg";
 import vitamin_water_img2_url from "/media/lil_nasx/2.jpg";
 import vitamin_water_img3_url from "/media/lil_nasx/3.jpg";
 
-  //? Kerwin Frost Show
-  import kerwin_frost_video_url from "/media/lil_nasx/lilnasx_kerwin_frost/lilnasx_kerwin_frost.mp4";
-  import kerwin_forst_img1_url from "/media/lil_nasx/lilnasx_kerwin_frost/1.png";
+//? Kerwin Frost Show
+import kerwin_frost_video_url from "/media/lil_nasx/lilnasx_kerwin_frost/lilnasx_kerwin_frost.mp4";
+import kerwin_forst_img1_url from "/media/lil_nasx/lilnasx_kerwin_frost/1.webp";
 
+//! TEEZO [MEDIA]
+//? Doja Cat - Masc!!
+import doja_cat_masc_video_url from "/media/teezo/teezo_doja_cat_masc.mp4";
 
-//? Carousel section Swiper styles import
+//? Coachela - Masc!!
+import coachela_masc_video_url from "/media/teezo/teezo_doja_cat_coachella_performance.mp4";
+import coachela_masc_img1_url from "/media/teezo/4.jpg";
+import coachela_masc_img2_url from "/media/teezo/5.webp";
+
+//? Apple Music!!
+import apple_music_img1_url from "/media/teezo/1.jpg";
+import apple_music_img2_url from "/media/teezo/2.jpg";
+import apple_music_img3_url from "/media/teezo/3.jpg";
+
+//? Exit Magazine!!
+import exit_mgzine_img1_url from "/media/teezo/6.jpg";
+
+//! DORIAN ELECTRA [MEDIA]
+//? My Agenda Tour!!
+import my_agenda_img1_url from "/media/dorian_electra/1.jpg";
+import my_agenda_img2_url from "/media/dorian_electra/2.jpg";
+import my_agenda_img3_url from "/media/dorian_electra/3.jpeg";
+
+//? My Agenda Videoclip!!
+import my_agenda_video_url from "/media/dorian_electra/dorian_electra_my_agenda_2021.mp4";
+
+//? Paper Magazine!!
+import paper_mag_img_url from "/media/dorian_electra/4.webp";
+
+//! MATTOX [MEDIA]
+//? Monterality!!
+import montreality_video_url from "/media/mattox/mattox_montreality.mp4";
+import montreality_img1_url from "/media/mattox/1.webp";
+import montreality_img2_url from "/media/mattox/2.webp";
+import montreality_img3_url from "/media/mattox/3.webp";
+import montreality_img4_url from "/media/mattox/4.webp";
+import montreality_img5_url from "/media/mattox/5.webp";
+import montreality_img6_url from "/media/mattox/6.webp";
+
+//! PAPERMAG [MEDIA]
+//? Interview!!
+import paper_mag_img1_url from "/media/paper_mag/1.webp";
+import paper_mag_img2_url from "/media/paper_mag/2.webp";
+import paper_mag_img3_url from "/media/paper_mag/3.webp";
+import paper_mag_img4_url from "/media/paper_mag/4.webp";
+import paper_mag_img5_url from "/media/paper_mag/5.webp";
+
+//! APEX [MEDIA]
+//? Custom Skates!!
+import custom_skates_img1_url from "/media/apex/1.webp";
+import custom_skates_img2_url from "/media/apex/2.webp";
+import custom_skates_img3_url from "/media/apex/3.webp";
+import custom_skates_img4_url from "/media/apex/4.webp";
+import custom_skates_img5_url from "/media/apex/5.webp";
+
+//! OTHERS [MEDIA]
+//? Costume Design 2024 I
+import costume_designI_video_url from "/media/others/babymonster.mp4";
+import costume_designI_img1_url from "/media/others/badvilain_1.webp";
+import costume_designI_img2_url from "/media/others/badvilain_2.webp";
+import costume_designI_img3_url from "/media/others/jinco_1.webp";
+import costume_designI_img4_url from "/media/others/jinco_2.webp";
+import costume_designI_img5_url from "/media/others/jinco_3.webp";
+import costume_designI_img6_url from "/media/others/moonchild_1.webp";
+import costume_designI_img7_url from "/media/others/moonchild_2.webp";
+import costume_designI_img8_url from "/media/others/moonchild_3.webp";
+import costume_designI_img9_url from "/media/others/xg_1.webp";
+import costume_designI_img10_url from "/media/others/xg_2.webp";
+
+//? Costume Design 2024 II
+
+//! Carousel section Swiper styles import
 const swiperStyles = `
 .swiper-button-next, .swiper-button-prev {
 color: #101828; /* Custom color for arrows */
@@ -60,15 +137,6 @@ margin: 0 5px !important;
 opacity: 1;
 }
 `;
-
-//? About section required parallax images import
-import parallax_leafs from "/media/about/parallax_leafs_cloud.png";
-import parallax_arm_blue from "/media/about/parallax_arm_blue.png";
-import parallax_park from "/media/about/parallax_park_cloud.png";
-import xavier_cloud from "/media/about/xavier_cloud.png";
-import arm from "/media/about/arm.png";
-import black_person_cut from "/media/about/black_person_cut.png";
-
 export const content = {
   heroContainer: {
     logoOnLight: racine_halftone,
@@ -232,29 +300,29 @@ export const content = {
             {
               subtitle: "Doja Cat - Masc",
               media: [
-                { type: "video", url: "/media/teezo/teezo_doja_cat_masc.mp4", title: "Teezo & Doja Cat - Masc" },
+                { type: "video", url: doja_cat_masc_video_url, title: "Teezo & Doja Cat - Masc" },
               ],
             },
             {
               subtitle: "Coachella - Masc",
               media: [
-                { type: "video", url: "/media/teezo/teezo_doja_cat_coachella_performance.mp4", title: "Teezo & Doja Cat - Masc (Coachella Performance)" },
-                { type: "image", url: "/media/teezo/4.jpg", title: "Teezo & Doja Cat - Masc (Coachella Performance)" },
-                { type: "image", url: "/media/teezo/5.jpg", title: "Teezo & Doja Cat - Masc (Coachella Performance)" },
+                { type: "video", url: coachela_masc_video_url, title: "Teezo & Doja Cat - Masc (Coachella Performance)" },
+                { type: "image", url: coachela_masc_img1_url, title: "Teezo & Doja Cat - Masc (Coachella Performance)" },
+                { type: "image", url: coachela_masc_img2_url, title: "Teezo & Doja Cat - Masc (Coachella Performance)" },
               ],
             },
             {
               subtitle: "Apple Music",
               media: [
-                { type: "image", url: "/media/teezo/1.jpg", title: "Teezo Touchdown - Apple Music" },
-                { type: "image", url: "/media/teezo/2.jpg", title: "Teezo Touchdown - Apple Music" },
-                { type: "image", url: "/media/teezo/3.jpg", title: "Teezo Touchdown - Apple Music" },
+                { type: "image", url: apple_music_img1_url, title: "Teezo Touchdown - Apple Music" },
+                { type: "image", url: apple_music_img2_url, title: "Teezo Touchdown - Apple Music" },
+                { type: "image", url: apple_music_img3_url, title: "Teezo Touchdown - Apple Music" },
               ],
             },
             {
               subtitle: "Exit Magazine",
               media: [
-                { type: "image", url: "/media/teezo/6.jpg", title: "Teezo Touchdown - Exit Magazine", },
+                { type: "image", url: exit_mgzine_img1_url, title: "Teezo Touchdown - Exit Magazine", },
               ],
             },
           ],
@@ -266,21 +334,21 @@ export const content = {
             {
               subtitle: "My Agenda Tour",
               media: [
-                { type: "image", url: "/media/dorian_electra/1.jpg", title: "Dorian Electra - My Agenda Tour" },
-                { type: "image", url: "/media/dorian_electra/2.jpg", title: "Dorian Electra - My Agenda Tour" },
-                { type: "image", url: "/media/dorian_electra/3.jpeg", title: "Dorian Electra - My Agenda Tour" },
+                { type: "image", url: my_agenda_img1_url, title: "Dorian Electra - My Agenda Tour" },
+                { type: "image", url: my_agenda_img2_url, title: "Dorian Electra - My Agenda Tour" },
+                { type: "image", url: my_agenda_img3_url, title: "Dorian Electra - My Agenda Tour" },
               ],
             },
             {
               subtitle: "My Agenda Videoclip",
               media: [
-                { type: "video", url: "/media/dorian_electra/dorian_electra_my_agenda_2021.mp4", title: "Dorian Electra - My Agenda Videoclip" },
+                { type: "video", url: my_agenda_video_url, title: "Dorian Electra - My Agenda Videoclip" },
               ],
             },
             {
               subtitle: "Paper Magazine",
               media: [
-                { type: "image", url: "/media/dorian_electra/4.png", title: "Dorian Electra - Paper Magazine" },
+                { type: "image", url: paper_mag_img_url, title: "Dorian Electra - Paper Magazine" },
               ],
             },
           ],
@@ -292,13 +360,13 @@ export const content = {
             {
               subtitle: "Montreality",
               media: [
-                { type: "video", url: "/media/mattox/mattox_montreality.mp4", title: "Mattox - Montreality" },
-                { type: "image", url: "/media/mattox/1.jpg", title: "Mattox - Montreality" },
-                { type: "image", url: "/media/mattox/2.jpg", title: "Mattox - Montreality" },
-                { type: "image", url: "/media/mattox/3.jpg", title: "Mattox - Montreality" },
-                { type: "image", url: "/media/mattox/4.png", title: "Mattox - Montreality" },
-                { type: "image", url: "/media/mattox/5.jpg", title: "Mattox - Montreality" },
-                { type: "image", url: "/media/mattox/6.jpg", title: "Mattox - Montreality" },
+                { type: "video", url: montreality_video_url, title: "Mattox - Montreality" },
+                { type: "image", url: montreality_img1_url, title: "Mattox - Montreality" },
+                { type: "image", url: montreality_img2_url, title: "Mattox - Montreality" },
+                { type: "image", url: montreality_img3_url, title: "Mattox - Montreality" },
+                { type: "image", url: montreality_img4_url, title: "Mattox - Montreality" },
+                { type: "image", url: montreality_img5_url, title: "Mattox - Montreality" },
+                { type: "image", url: montreality_img6_url, title: "Mattox - Montreality" },
               ],
             },
           ],
@@ -310,11 +378,11 @@ export const content = {
             {
               subtitle: "Interview",
               media: [
-                { type: "image", url: "/media/paper_mag/1.jpg", title: "Paper Magazine - Princess Gollum", name: "Princess Gollum" },
-                { type: "image", url: "/media/paper_mag/2.jpg", title: "Paper Magazine - Lil Texas", name: "Lil Texas" },
-                { type: "image", url: "/media/paper_mag/3.jpg", title: "Paper Magazine - Dorian Electra", name: "Dorian Electra" },
-                { type: "image", url: "/media/paper_mag/4.jpg", title: "Paper Magazine - Dizzy Fae", name: "Dizzy Fae", },
-                { type: "image", url: "/media/paper_mag/5.jpg", title: "Paper Magazine - Paolo Perfeccion", name: "Paolo Perfeccion" },
+                { type: "image", url: paper_mag_img1_url, title: "Paper Magazine - Princess Gollum", name: "Princess Gollum" },
+                { type: "image", url: paper_mag_img2_url, title: "Paper Magazine - Lil Texas", name: "Lil Texas" },
+                { type: "image", url: paper_mag_img3_url, title: "Paper Magazine - Dorian Electra", name: "Dorian Electra" },
+                { type: "image", url: paper_mag_img4_url, title: "Paper Magazine - Dizzy Fae", name: "Dizzy Fae", },
+                { type: "image", url: paper_mag_img5_url, title: "Paper Magazine - Paolo Perfeccion", name: "Paolo Perfeccion" },
               ],
             },
           ],
@@ -326,33 +394,53 @@ export const content = {
             {
               subtitle: "Custom Skates",
               media: [
-                { type: "image", url: "/media/apex/1.jpg", title: "Apex Custom Skates" },
-                { type: "image", url: "/media/apex/2.jpg", title: "Apex Custom Skates" },
-                { type: "image", url: "/media/apex/3.jpg", title: "Apex Custom Skates" },
-                { type: "image", url: "/media/apex/4.jpg", title: "Apex Custom Skates" },
-                { type: "image", url: "/media/apex/5.jpg", title: "Apex Custom Skates" },
+                { type: "image", url: custom_skates_img1_url, title: "Apex Custom Skates" },
+                { type: "image", url: custom_skates_img2_url, title: "Apex Custom Skates" },
+                { type: "image", url: custom_skates_img3_url, title: "Apex Custom Skates" },
+                { type: "image", url: custom_skates_img4_url, title: "Apex Custom Skates" },
+                { type: "image", url: custom_skates_img5_url, title: "Apex Custom Skates" },
               ],
             },
           ],
         },
         {
           id: "others",
-          title: "Others",
+          title: "Costume Design",
           projects: [
             {
-              subtitle: "Full Costume Design 2024",
+              subtitle: "Baby  Monster",
               media: [
-                { type: "video", url: "/media/others/babymonster.mp4", title: "Costume Design - Baby Monster", name: "Baby Monster" },
-                { type: "image", url: "/media/others/badvilain_1.jpg", title: "Costume Design - Bad Villain", name: "Baby Monster" },
-                { type: "image", url: "/media/others/badvilain_2.png", title: "Costume Design - Bad Villain", name: "Baby Monster" },
-                { type: "image", url: "/media/others/jinco_1.jpg", title: "Costume Design - Jinco", name: "Jinco" },
-                { type: "image", url: "/media/others/jinco_2.jpg", title: "Costume Design - Jinco", name: "Jinco" },
-                { type: "image", url: "/media/others/jinco_3.jpg", title: "Costume Design - Jinco", name: "Jinco" },
-                { type: "image", url: "/media/others/moonchild_1.jpg", title: "Costume Design - Moonchild", name: "Moonchild" },
-                { type: "image", url: "/media/others/moonchild_2.jpg", title: "Costume Design - Moonchild", name: "Moonchild" },
-                { type: "image", url: "/media/others/moonchild_3.jpg", title: "Costume Design - Moonchild", name: "Moonchild" },
-                { type: "image", url: "/media/others/xg_1.jpg", title: "Costume Design - XG", name: "XG" },
-                { type: "image", url: "/media/others/xg_2.jpg", title: "Costume Design - XG", name: "XG" },
+                { type: "video", url: costume_designI_video_url, title: "Costume Design - Baby Monster", name: "Costume Design 2024" },
+              ],
+            },
+            {
+              subtitle: "Bad  Villain",
+              media: [
+                { type: "image", url: costume_designI_img1_url, title: "Costume Design - Bad Villain", name: "Bad Villain" },
+                { type: "image", url: costume_designI_img2_url, title: "Costume Design - Bad Villain", name: "Bad Villain" },
+              ],
+            },
+            {
+              subtitle: "Jinco",
+              media: [
+                { type: "image", url: costume_designI_img3_url, title: "Costume Design - Jinco", name: "Jinco" },
+                { type: "image", url: costume_designI_img4_url, title: "Costume Design - Jinco", name: "Jinco" },
+                { type: "image", url: costume_designI_img5_url, title: "Costume Design - Jinco", name: "Jinco" },
+              ],
+            },
+            {
+              subtitle: "Moonchild",
+              media: [
+                { type: "image", url: costume_designI_img6_url, title: "Costume Design - Moonchild", name: "Moonchild" },
+                { type: "image", url: costume_designI_img7_url, title: "Costume Design - Moonchild", name: "Moonchild" },
+                { type: "image", url: costume_designI_img8_url, title: "Costume Design - Moonchild", name: "Moonchild" },
+              ],
+            },
+            {
+              subtitle: "XG",
+              media: [
+                { type: "image", url: costume_designI_img9_url, title: "Costume Design - XG", name: "XG" },
+                { type: "image", url: costume_designI_img10_url, title: "Costume Design - XG", name: "XG" },
               ],
             },
           ],
