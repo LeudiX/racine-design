c60123c0120f2aba5e10a054015c857961315deb
LeudiX <leudix.rafael@gmail.com>
2025-03-26 20:03:27 -0400

    feat(vite): Updated vite config for prerelease

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base:"/racine-design",
+  base:"/emil3v3-react-ts-horizontal",
 })
