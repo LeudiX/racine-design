2d2a85b766f158162a92edd5a95df05c8e447efb
LeudiX <leudix.rafael@gmail.com>
2025-03-29 20:56:25 -0400

    feat(vite): Added config for SEO optimization over app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import sitemap  from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-  base:"/racine-design",
+  plugins: [tailwindcss(), react(),
+  sitemap({
+    hostname: 'https://leudix.github.io/',
+    dynamicRoutes: ['/racine-design'], // Add all your routes
+  }),],
+  base: "/racine-design",
 })
