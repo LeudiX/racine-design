f19c286d2665e2200ca67782010b672d22cb5d62
LeudiX <leudix.rafael@gmail.com>
2025-04-07 15:40:31 -0400

    feat(App): Added custom states for tracking scrolling and transition events.Added polyfill library for better browser compatibility. Updated logic in scrollToSection function for transition handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { polyfill } from 'smoothscroll-polyfill'
 import React, { useState, useEffect, useRef } from 'react';
 import HorizontalScrollContainer from './components/shared/HorizontalScrollContainer'
 import HeroContainer from './components/HeroContainer'
@@ -8,7 +9,6 @@ import About from './components/About'
 import SchemaMarkup from './components/seo/SchemaMarkup';
 
 const App: React.FC = () => {
-
   // Dark Mode Handling
   const [isDarkMode, setIsDarkMode] = useState(false);
 
@@ -21,6 +21,12 @@ const App: React.FC = () => {
   // Ref to track if the project change originated from a subtitle click
   const isSubtitleClickRef = useRef(false);
 
+  //State to track the scrolling of the system in real time
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  //State for track the transition between sections
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
   // Handle theme change
   const handleThemeChange = (isDarkMode: boolean) => {
     setIsDarkMode(isDarkMode);
@@ -31,21 +37,45 @@ const App: React.FC = () => {
     const container = document.getElementById("horizontal-scroll-container");
     if (section && container) {
 
+      //Enable scrolling
+      setIsScrolling(true);
+      // Start transition
+      setIsTransitioning(true);
+
+      // Add initial blur state
+      container.classList.add('backdrop-blur-sm');
+
       // Reset the vertical scroll position so that the section is a aligned at the top.
       section.scrollTop = 0;
 
       // Get the section’s left offset relative to the container
       const sectionLeft = section.offsetLeft;
 
-      // Use scrollIntoView to smoothly scroll the section into view
-      container.scrollTo({
-        left: sectionLeft,
-        behavior: "smooth",  // Aligns the section at the top of the viewport
-      });
+      // Start complex transition
+      setTimeout(() => {
+        container.classList.replace('backdrop-blur-sm', 'backdrop-blur-md');
+        // Scroll after short delay
+        setTimeout(() => {
+          // Use scrollIntoView to smoothly scroll the section into view
+          container.scrollTo({
+            left: sectionLeft,
+            behavior: "smooth",  // Aligns the section at the top of the viewport
+          });
+        }, 500);
+      }, 100);
+
+      // End transition after animation
+      setTimeout(() => {
+        container.classList.remove('backdrop-blur-md', 'backdrop-blur-sm');
+        setIsTransitioning(false);
+        setIsScrolling(false);
+      }, 1500);
     }
   };
 
   useEffect(() => {
+    polyfill(); //Ensure smooth scroll behavior between multiple browser devices
+
     if (!isSubtitleClickRef.current) {
       setActiveSubtitleIndex(0); // Only reset subtitle index for project portrait button clicks
     }
@@ -65,8 +95,8 @@ const App: React.FC = () => {
 
   return (
     <>
-      <SchemaMarkup/>
-      <HorizontalScrollContainer>
+      <SchemaMarkup />
+      <HorizontalScrollContainer isScrolling={isScrolling} setIsScrolling={setIsScrolling} isTransitioning={isTransitioning} setIsTransitioning={setIsTransitioning} isDarkMode={isDarkMode}>
         {/* Pass isDarkMode and handleThemeChange to HeroContainer */}
         <HeroContainer scrollToSection={scrollToSection} isDarkMode={isDarkMode} onThemeChange={handleThemeChange} />
 
