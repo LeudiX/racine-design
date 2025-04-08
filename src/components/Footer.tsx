190088a6bde0501a52430682d7067126f755bbc9
LeudiX <leudix.rafael@gmail.com>
2025-04-07 23:12:19 -0400

    feat(Footer): Added custom padding and mt properties

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="font-[Kanit] text-xs md:text-sm font-medium scale-y-90">
-            <h6 className="mb-3 ">Émile Racine</h6>
+        <footer className="font-[Kanit] text-xs md:text-sm font-medium scale-y-90 pb-5 -mt-2 md:mt-12">
+            <h6>Émile Racine</h6>
             <p>designbyracine © All CopyRights Reserved 2025</p>
         </footer>
     )
