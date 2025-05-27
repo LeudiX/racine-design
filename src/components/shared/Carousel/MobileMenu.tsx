da6d4a4c35a0e7271eb91bf1e154bf743bd9bbe9
LeudiX <leudix.rafael@gmail.com>
2025-05-27 11:35:26 -0400

    fix(MobileMenu): Enhanced logic with additional props for a better activeProject tracking

--- src/components/shared/Carousel/MobileMenu.tsx
+++ src/components/shared/Carousel/MobileMenu.tsx
@@ -9,20 +9,22 @@ interface MobileMenuProps {
     isSidebarOpen: boolean;
     setIsSidebarOpen: (isSidebarOpen: boolean) => void;
     projects: readonly Project[];
-    onButtonClick: (projectId: string) => void;
+    activeProject: Project | null;
+    setActiveProject: (project: Project | null) => void;
+    onSubtitleClick: (projectId: string, subtitleIndex: number) => void;
     className: string;
 }
 
 // Dataset instance for carousel section
 const { carousel } = content;
 
-const MobileMenu: React.FC<MobileMenuProps> = ({ isSidebarOpen, setIsSidebarOpen, projects, onButtonClick, className }) => {
+const MobileMenu: React.FC<MobileMenuProps> = ({ isSidebarOpen, setIsSidebarOpen, projects, setActiveProject, onSubtitleClick, className }) => {
     return (
         <div className={`${carousel.projects.className} ${className}`}>
             {projects.slice(0, 5).map((project) => (
                 <button
                     key={project.id}
-                    onClick={() => onButtonClick(project.id)} // Updated handler
+                    onClick={() =>{setActiveProject(project); onSubtitleClick(project.id,0)}} // Updated handler
                     className={carousel.button.className}
                 >
                     {project.title} {/*Proyect 1st artist's name*/}
@@ -30,7 +32,7 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ isSidebarOpen, setIsSidebarOpen
 
             ))}
             {/*Sidebar Button Toggle*/}
-            <button onClick={() => setIsSidebarOpen(!isSidebarOpen)} className={carousel.button.className}>
+            <button onClick={() => { setIsSidebarOpen(!isSidebarOpen) }} className={carousel.button.className}>
                 more
                 {isSidebarOpen ? (
                     <XMarkIcon aria-hidden="true" className="-mr-3 size-4 inline-block" />
